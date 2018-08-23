41f302625d7c8d4e23fc55562b8f9f30c1b06628
ekachaitose <ekachaitose11@gmail.com>
2018-08-23 23:10:35 +0700

    add routes and function login

--- db/db.js
+++ db/db.js
@@ -1,8 +1,504 @@
-const todos = [
+const USERS = [
   {
     id: 1,
-    title: 'lunch',
-    desciption: 'Go for lunch by 2pm'
+    username: 'Martina',
+    password: '1214'
+  },
+  {
+    id: 2,
+    username: 'Grace',
+    password: '1224'
+  },
+  {
+    id: 3,
+    username: 'Dillon',
+    password: '1234'
+  },
+  {
+    id: 4,
+    username: 'Abelardo',
+    password: '1244'
+  },
+  {
+    id: 5,
+    username: 'Ken',
+    password: '1254'
+  },
+  {
+    id: 6,
+    username: 'Al',
+    password: '1264'
+  },
+  {
+    id: 7,
+    username: 'Daisy',
+    password: '1274'
+  },
+  {
+    id: 8,
+    username: 'Felicity',
+    password: '1284'
+  },
+  {
+    id: 9,
+    username: 'Alden',
+    password: '1294'
+  },
+  {
+    id: 10,
+    username: 'Luz',
+    password: '12104'
+  },
+  {
+    id: 11,
+    username: 'Francesco',
+    password: '12114'
+  },
+  {
+    id: 12,
+    username: 'Myron',
+    password: '12124'
+  },
+  {
+    id: 13,
+    username: 'Rocio',
+    password: '12134'
+  },
+  {
+    id: 14,
+    username: 'Arnold',
+    password: '12144'
+  },
+  {
+    id: 15,
+    username: 'Celestine',
+    password: '12154'
+  },
+  {
+    id: 16,
+    username: 'Skyla',
+    password: '12164'
+  },
+  {
+    id: 17,
+    username: 'Loren',
+    password: '12174'
+  },
+  {
+    id: 18,
+    username: 'Elsie',
+    password: '12184'
+  },
+  {
+    id: 19,
+    username: 'Bette',
+    password: '12194'
+  },
+  {
+    id: 20,
+    username: 'Alejandra',
+    password: '12204'
+  },
+  {
+    id: 21,
+    username: 'Elmer',
+    password: '12214'
+  },
+  {
+    id: 22,
+    username: 'Demetrius',
+    password: '12224'
+  },
+  {
+    id: 23,
+    username: 'Evie',
+    password: '12234'
+  },
+  {
+    id: 24,
+    username: 'Vallie',
+    password: '12244'
+  },
+  {
+    id: 25,
+    username: 'Mackenzie',
+    password: '12254'
+  },
+  {
+    id: 26,
+    username: 'Celia',
+    password: '12264'
+  },
+  {
+    id: 27,
+    username: 'Mckenna',
+    password: '12274'
+  },
+  {
+    id: 28,
+    username: 'Berenice',
+    password: '12284'
+  },
+  {
+    id: 29,
+    username: 'Sheila',
+    password: '12294'
+  },
+  {
+    id: 30,
+    username: 'Haylie',
+    password: '12304'
+  },
+  {
+    id: 31,
+    username: 'Liza',
+    password: '12314'
+  },
+  {
+    id: 32,
+    username: 'Emory',
+    password: '12324'
+  },
+  {
+    id: 33,
+    username: 'Kiel',
+    password: '12334'
+  },
+  {
+    id: 34,
+    username: 'Shanon',
+    password: '12344'
+  },
+  {
+    id: 35,
+    username: 'Eva',
+    password: '12354'
+  },
+  {
+    id: 36,
+    username: 'Chaya',
+    password: '12364'
+  },
+  {
+    id: 37,
+    username: 'Trisha',
+    password: '12374'
+  },
+  {
+    id: 38,
+    username: 'Cloyd',
+    password: '12384'
+  },
+  {
+    id: 39,
+    username: 'Savanah',
+    password: '12394'
+  },
+  {
+    id: 40,
+    username: 'Nathanael',
+    password: '12404'
+  },
+  {
+    id: 41,
+    username: 'Kaylie',
+    password: '12414'
+  },
+  {
+    id: 42,
+    username: 'Cydney',
+    password: '12424'
+  },
+  {
+    id: 43,
+    username: 'Lane',
+    password: '12434'
+  },
+  {
+    id: 44,
+    username: 'Markus',
+    password: '12444'
+  },
+  {
+    id: 45,
+    username: 'Graham',
+    password: '12454'
+  },
+  {
+    id: 46,
+    username: 'Lucius',
+    password: '12464'
+  },
+  {
+    id: 47,
+    username: 'Santina',
+    password: '12474'
+  },
+  {
+    id: 48,
+    username: 'Jodie',
+    password: '12484'
+  },
+  {
+    id: 49,
+    username: 'Christian',
+    password: '12494'
+  },
+  {
+    id: 50,
+    username: 'Elinor',
+    password: '12504'
+  },
+  {
+    id: 51,
+    username: 'Stanford',
+    password: '12514'
+  },
+  {
+    id: 52,
+    username: 'Zack',
+    password: '12524'
+  },
+  {
+    id: 53,
+    username: 'Ben',
+    password: '12534'
+  },
+  {
+    id: 54,
+    username: 'Wilhelm',
+    password: '12544'
+  },
+  {
+    id: 55,
+    username: 'Annie',
+    password: '12554'
+  },
+  {
+    id: 56,
+    username: 'Karine',
+    password: '12564'
+  },
+  {
+    id: 57,
+    username: 'Melvina',
+    password: '12574'
+  },
+  {
+    id: 58,
+    username: 'Kim',
+    password: '12584'
+  },
+  {
+    id: 59,
+    username: 'Phyllis',
+    password: '12594'
+  },
+  {
+    id: 60,
+    username: 'Dovie',
+    password: '12604'
+  },
+  {
+    id: 61,
+    username: 'Tess',
+    password: '12614'
+  },
+  {
+    id: 62,
+    username: 'Glennie',
+    password: '12624'
+  },
+  {
+    id: 63,
+    username: 'Amira',
+    password: '12634'
+  },
+  {
+    id: 64,
+    username: 'Willa',
+    password: '12644'
+  },
+  {
+    id: 65,
+    username: 'Citlalli',
+    password: '12654'
+  },
+  {
+    id: 66,
+    username: 'Bert',
+    password: '12664'
+  },
+  {
+    id: 67,
+    username: 'Leora',
+    password: '12674'
+  },
+  {
+    id: 68,
+    username: 'Stacey',
+    password: '12684'
+  },
+  {
+    id: 69,
+    username: 'Eldon',
+    password: '12694'
+  },
+  {
+    id: 70,
+    username: 'Rosa',
+    password: '12704'
+  },
+  {
+    id: 71,
+    username: 'Elinore',
+    password: '12714'
+  },
+  {
+    id: 72,
+    username: 'Raquel',
+    password: '12724'
+  },
+  {
+    id: 73,
+    username: 'Julio',
+    password: '12734'
+  },
+  {
+    id: 74,
+    username: 'Shanelle',
+    password: '12744'
+  },
+  {
+    id: 75,
+    username: 'Gianni',
+    password: '12754'
+  },
+  {
+    id: 76,
+    username: 'Maia',
+    password: '12764'
+  },
+  {
+    id: 77,
+    username: 'Alayna',
+    password: '12774'
+  },
+  {
+    id: 78,
+    username: 'Immanuel',
+    password: '12784'
+  },
+  {
+    id: 79,
+    username: 'Odessa',
+    password: '12794'
+  },
+  {
+    id: 80,
+    username: 'Harold',
+    password: '12804'
+  },
+  {
+    id: 81,
+    username: 'Amir',
+    password: '12814'
+  },
+  {
+    id: 82,
+    username: 'Duncan',
+    password: '12824'
+  },
+  {
+    id: 83,
+    username: 'Alf',
+    password: '12834'
+  },
+  {
+    id: 84,
+    username: 'Stephen',
+    password: '12844'
+  },
+  {
+    id: 85,
+    username: 'Jordi',
+    password: '12854'
+  },
+  {
+    id: 86,
+    username: 'Buck',
+    password: '12864'
+  },
+  {
+    id: 87,
+    username: 'Rosemarie',
+    password: '12874'
+  },
+  {
+    id: 88,
+    username: 'Makayla',
+    password: '12884'
+  },
+  {
+    id: 89,
+    username: 'Veda',
+    password: '12894'
+  },
+  {
+    id: 90,
+    username: 'Winifred',
+    password: '12904'
+  },
+  {
+    id: 91,
+    username: 'Lambert',
+    password: '12914'
+  },
+  {
+    id: 92,
+    username: 'Tierra',
+    password: '12924'
+  },
+  {
+    id: 93,
+    username: 'Alvis',
+    password: '12934'
+  },
+  {
+    id: 94,
+    username: 'Clare',
+    password: '12944'
+  },
+  {
+    id: 95,
+    username: 'Luella',
+    password: '12954'
+  },
+  {
+    id: 96,
+    username: 'Davonte',
+    password: '12964'
+  },
+  {
+    id: 97,
+    username: 'Cody',
+    password: '12974'
+  },
+  {
+    id: 98,
+    username: 'Zane',
+    password: '12984'
+  },
+  {
+    id: 99,
+    username: 'Joanne',
+    password: '12994'
+  },
+  {
+    id: 100,
+    username: 'Brigitte',
+    password: '121004'
   }
 ]
-export default todos
+
+export default USERS
